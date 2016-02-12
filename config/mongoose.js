c1c86e90f8c143f1d46e504581c22ab47a3072de
Mathew Liew <mliew21396@gmail.com>
2016-02-12 11:09:45 -0600

    Add mongoose heroku URI

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,8 @@ var config = require('./config'),
 	mongoose = require('mongoose');
 
 module.exports = function() {
-	var db = mongoose.connect(config.db);
+	var db = mongoose.connect(process.env.MONGOLAB_URI || config.db);
+  // mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/meals-development');
 
 	require('../app/models/user.server.model');
 	require('../app/models/post.server.model');
