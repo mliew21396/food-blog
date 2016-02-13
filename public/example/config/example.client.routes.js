65c8f4af79a1f81f3cfe11c994531ef61134c2d1
Mathew Liew <mliew21396@gmail.com>
2016-02-13 14:52:53 -0600

    Create partials for header

--- public/example/config/example.client.routes.js
+++ public/example/config/example.client.routes.js
@@ -2,7 +2,7 @@ angular.module('example').config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider.
 		when('/', {
-			templateUrl: 'example/views/example.client.view.html'
+      templateUrl: 'example/views/example.client.view.html'
 		}).
 		otherwise({
 			redirectTo: '/'
