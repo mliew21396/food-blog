5fa509909b8ffd8c0f354dd7f353c4061f7bd4ba
Mathew Liew <mliew21396@gmail.com>
2016-02-13 11:14:05 -0600

    Add other contributor easy setup and prod variables

--- config/env/production.js
+++ config/env/production.js
@@ -2,5 +2,5 @@ var port = 1337;
 
 module.exports = {
 	port: process.env.PORT || port,
-	db: 'mongodb://heroku_1p6bf39c:cpb3sv3o5kg13f710lddplhdl8@ds061355.mongolab.com:61355/heroku_1p6bf39c'
+	db: 'MONGOLAB_URI'
 };
